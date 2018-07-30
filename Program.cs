1ca2df42835e21c580d11acece63b43eeb101591
Cleber Takahiro Shiguematsu <cleber.takahiro@gmail.com>
2018-07-30 18:32:17 -0300

    Primeira versão

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicio
+            Console.WriteLine("Hello World!");
+
+            var s = "Good morning!";
+
+            Console.WriteLine(s);
+
+            Console.WriteLine($"Today is {Util.Data()}");
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    public class Util
+    {
+        public static string Data()
+        {
+            // retorna data
+            return DateTime.Now.ToShortDateString();
+        }
+    }
+}
